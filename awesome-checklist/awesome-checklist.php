99c95c06914cbc6c1e8a9f11412450f14249555b
mx <max@onebro.me>
2016-03-17 21:00:20 +1300

    Updated description

--- awesome-checklist/awesome-checklist.php
+++ awesome-checklist/awesome-checklist.php
@@ -1,8 +1,8 @@
 <?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 /**
  * Plugin Name: Awesome Checklist
- * Plugin URI: http://github.com/ysupr/awesome-checklist
- * Description: Convert ul and li tags to became checklist for visitors
+ * Plugin URI: https://github.com/mVentory/utils/tree/master/awesome-checklist
+ * Description: Converts ul (add class="mvchecklist") and li (add id="[unique str]") tags into reusable checklists for visitors.
  * Version: 1.0.0
  * Author: Yoga Sukma
  * Author URI: http://ysupr.net
